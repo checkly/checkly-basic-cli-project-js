b03b713a3b635aa85a07f44c8309f73eb6c02481
jonathan <joncanales2@gmail.com>
2023-05-29 21:02:21 -0400

    removed defaults file.

--- __checks__/defaults.js
+++
@@ -1,8 +0,0 @@
-const defaults = {
-  pageUrl: 'https://danube-web.shop', // the pageUrl can be replaced by urls from a preview / staging environment.
-  playwright: {
-    viewportSize: { width: 1920, height: 1080 },
-  },
-};
-
-module.export = defaults;
--- __checks__/homepage.spec.js
+++ __checks__/homepage.spec.js
@@ -1,12 +1,11 @@
-const { test, expect } = require('@playwright/test') 
-const { defaults } = require('./defaults');
+const { test, expect } = require('@playwright/test');
 
 // You can override the default Playwright test timeout of 30s
 // test.setTimeout(60_000);
 
 test('Checkly Homepage', async ({ page }) => {
-  const response = await page.goto(`${defaults.pageUrl}`)
-  expect(response?.status()).toBeLessThan(400)
-  await expect(page).toHaveTitle(/Danube WebShop/)
-  await page.screenshot({ path: 'homepage.jpg' })
-})
+  const response = await page.goto('https://danube-web.shop');
+  expect(response?.status()).toBeLessThan(400);
+  await expect(page).toHaveTitle(/Danube WebShop/);
+  await page.screenshot({ path: 'homepage.jpg' });
+});
